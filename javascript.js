caf7c36ff5c5e7dd64d79fae68b25cf4a9a46812
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-03-21 20:28:13 -0600

    Add new grid button and button functionality
    When clicked button resets grid and generates a new one based on
    user input

--- javascript.js
+++ javascript.js
@@ -1,18 +1,47 @@
-const gridContainer = document.querySelector(".container")
+const gridContainer = document.querySelector(".container");
 
 function gridCreate() {
     for (i = 0; i < 256; i++) {
         const gridSquare = document.createElement("div");
         gridSquare.classList.add("grid-square");
+        gridSquare.style.width = (100 / 16) + "%";
         gridContainer.appendChild(gridSquare);
     }
     
 }
 gridCreate();
 
-const drawFunction = document.querySelectorAll(".grid-square")
+const drawFunction = document.querySelectorAll(".grid-square");
 for (let div of drawFunction) {
     div.addEventListener("mouseenter", () => {
         div.style.backgroundColor = 'royalblue'
     })
 }
+
+const newGridButton = document.querySelector("#new-grid");
+newGridButton.addEventListener("click", () => {
+    let getNewGridValue = prompt("What would you like the size of your new grid to be?");
+    let newGridValue = parseInt(getNewGridValue);
+
+    if (newGridValue <= 100) {
+    let newGridSize = newGridValue * newGridValue;
+
+    gridContainer.replaceChildren();
+
+    for (i = 0; i < newGridSize; i++) {
+        const gridSquare = document.createElement("div");
+        gridSquare.classList.add("grid-square");
+        gridSquare.style.width = (100 / newGridValue) + "%";
+        gridContainer.appendChild(gridSquare);
+    }
+    } else {
+        alert("Value selected too high!");
+    }
+    const drawFunction = document.querySelectorAll(".grid-square");
+for (let div of drawFunction) {
+    div.addEventListener("mouseenter", () => {
+        div.style.backgroundColor = 'royalblue'
+    })
+}
+}
+)
