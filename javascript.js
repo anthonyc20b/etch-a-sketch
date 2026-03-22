cb1892a7b2c6fd2daeba1f75514c16f03c280907
Anthony Cifaloglio <anthonyc20b@gmail.com>
2026-03-21 19:44:38 -0600

    Add grid with appropriate sizing and scaling

--- javascript.js
+++ javascript.js
@@ -1,13 +1,12 @@
 const gridContainer = document.querySelector(".container")
 
 function gridCreate() {
-    for (i = 0; i < 255; i++) {
+    for (i = 0; i < 256; i++) {
         const gridSquare = document.createElement("div");
-        gridSquare.textContent = "Test";
         gridSquare.classList.add("grid-square");
         gridContainer.appendChild(gridSquare);
     }
     
 }
-
 gridCreate();
+
